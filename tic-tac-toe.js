8b3852fc9ac4572a57c4f43a7a37811b90e24846
Suzanne Harrison <harrison_family@hotmail.com>
2016-06-30 13:37:39 -0700

    renders xs and o s now and switches turns.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,21 +8,41 @@ function TicTacToe() {
 }
 
 TicTacToe.prototype.manageBoard = function(square) {
+  console.log(this.currentPlayer)
   switch ($(square).attr('id')) {
     case "top-left":
         this.board[0][0] = this.currentPlayer
         break;
+    case "top-center":
+        this.board[0][1] = this.currentPlayer
+        break;
+    case "top-right":
+        this.board[0][2] = this.currentPlayer
+        break;
+    case "middle-left":
+        this.board[1][0] = this.currentPlayer
+        break;
+    case "middle-center":
+        this.board[1][1] = this.currentPlayer
+        break;
+    case "middle-right":
+        this.board[1][2] = this.currentPlayer
+        break;
+    case "bottom-left":
+        this.board[2][0] = this.currentPlayer
+        break;
+    case "bottom-center":
+        this.board[2][1] = this.currentPlayer
+        break;
+    case "bottom-right":
+        this.board[2][2] = this.currentPlayer
+        break;
   }
 }
 
-TicTacToe.prototype.playGame = {
-  //player
-
-}
-
-TicTacToe.prototype.takeTurn = {
-  //player
-
+TicTacToe.prototype.play = function() {
+  //player switch
+  this.switchPlayer();
 }
 
 TicTacToe.prototype.switchPlayer = function() {
@@ -33,27 +53,29 @@ TicTacToe.prototype.switchPlayer = function() {
   }
 }
 
-// TicTacToe.prototype.checkWin = function() {
-//   //check win/loss/draw
-//
-// }
-//
-// TicTacToe.prototype.postgameCommunication = function() {
-//   //communicate outcome
-//
-// }
-//
-// TicTacToe.prototype.playAgain = function() {
-//   //offer to play again
-//
-// }
+TicTacToe.prototype.checkWin = function() {
+  //check win/loss/draw
+
+}
+
+TicTacToe.prototype.postgameCommunication = function() {
+  //communicate outcome
+
+}
+
+TicTacToe.prototype.playAgain = function() {
+  //offer to play again
+
+}
 
 //jQuery
 $(document).ready(function() {
   console.log('create and begin the game here!')
   var game = new TicTacToe()
   $('.square').one('click', function() {
-    $(this).append( "<p>Test</p>" )
+    console.log(game.currentPlayer)
+    $(this).append( "<p>" + game.currentPlayer + "</p>" )
+    game.manageBoard(this)
     game.play()
   })
 })
